b9cc2a04be5ee6be31087f3f101c6c91001a4e4f
yesak1 <23kkazliner@athenian.org>
2020-09-09 22:58:15 +0000

    finished, ready to commit

--- countdown.py
+++ countdown.py
@@ -1,3 +1,4 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
-
+for x in range(10,21):
+    print(x)
--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,11 @@
 # Collaborators: 
 #
+import random
+for x in range(1):
+    number=random.randint(2,50)
+    print (number)
+if number % 3:
+    print (f"{number} is not divisible by three")
+else:
+    print ((f"{number} is divisible by three"))
 
