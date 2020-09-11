2a39506e0098dc6bc4ad1816cfca19264f9d22fe
yesak1 <23kkazliner@athenian.org>
2020-09-11 17:01:49 +0000

    finished, update 9.11

--- countdown.py
+++ countdown.py
@@ -1,4 +1,5 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
-for x in range(10,21):
+#  none
+for x in range(20,10,-1):
     print(x)
+
--- divisibility_check.py
+++ divisibility_check.py
@@ -1,11 +1,10 @@
 # Collaborators: 
 #
-import random
-for x in range(1):
-    number=random.randint(2,50)
-    print (number)
-if number % 3:
-    print (f"{number} is not divisible by three")
-else:
-    print ((f"{number} is divisible by three"))
+
+for x in range(2,51):
+    
+    if x % 3==0:
+        print (f"{x} is divisible by three")
+    else:
+        print ((f"{x} is not divisible by three"))
 
